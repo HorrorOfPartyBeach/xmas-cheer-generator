52107dd43e699841a85e65e6d61efb9bee5a5223
HorrorOfPartyBeach <e.gilmour33@gmail.com>
2019-11-21 21:09:18 +0000

    commit build folder

---
+++ build/precache-manifest.2ac9efacdc4c53b2fd65d32dc9041adc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955fc76b280f82d2ab864821047f6c22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef16d2103c6f88fc9281",
+    "url": "/static/css/main.05135efb.chunk.css"
+  },
+  {
+    "revision": "1cac29b14443246778b2",
+    "url": "/static/js/2.6ee67f2e.chunk.js"
+  },
+  {
+    "revision": "ef16d2103c6f88fc9281",
+    "url": "/static/js/main.1118e8ee.chunk.js"
+  },
+  {
+    "revision": "c7323af5211a54587120",
+    "url": "/static/js/runtime-main.ac48e3db.js"
+  }
+]);
